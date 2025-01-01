9c748a5c0f66ad6f769267b8210bbadee00c9731
Durdonaxon95 <qilichovadurdona7@gmail.com>
2025-01-01 20:54:04 +0500

    INFRA: UPLOAD PROJECT

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using System;
+int birinchison = Convert.ToInt32(Console.ReadLine());
+int ikkinchison = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"Qo'shish {birinchison+ikkinchison}");
