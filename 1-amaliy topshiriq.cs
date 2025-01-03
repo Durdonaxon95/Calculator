e712475f97b2c9ff42eb058cc3016226282af317
Durdonaxon95 <qilichovadurdona7@gmail.com>
2025-01-03 20:53:49 +0500

    teach-comparison

--- 1-amaliy topshiriq.cs
+++ 1-amaliy topshiriq.cs
@@ -11,5 +11,19 @@ partial class Program
 		double doiraperimetri = 2 * pi * radius;
 		System.Console.WriteLine($"Doira yuzi: S= {doirayuzi}");
 		System.Console.WriteLine($"Doira perimetri: P= {doiraperimetri}");
+		 
+		 //Dollar kursini hisoblash
+		 Console.WriteLine("Qiymatni kiriting: ");
+		 double qiymat = Convert.ToDouble(Console.ReadLine());
+		 double dollar = 12400*qiymat;
+		 System.Console.WriteLine($"Dollar kursi: {dollar} so'm");
+
+		 //Foydalanuvchi yoshini kunlarda aniqlash
+		 Console.WriteLine("Tug'ilgan yilingizni kiriting: ");
+		 int yil= Convert.ToInt32(Console.ReadLine());
+		 int yosh = DateTime.Now.Year - yil;
+		 int kun = yosh * 365;
+		 System.Console.WriteLine($"Siz {kun} kun yashadingiz");
+
 	}
 }
