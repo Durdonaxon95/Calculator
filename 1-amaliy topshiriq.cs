70c03fc88ac96285ff0aebcc882326a4da1c6762
Durdonaxon95 <qilichovadurdona7@gmail.com>
2025-01-03 20:34:15 +0500

    teach-comparison

--- 1-amaliy topshiriq.cs
+++ 1-amaliy topshiriq.cs
@@ -5,7 +5,7 @@ partial class Program
 	static void Main()
 	{
 		Console.WriteLine("Doira radiusini kiriting: ");
-		double radius = Convert.ToInt32(Console.ReadLine());
+		double radius =Convert.ToDouble(Console.ReadLine());
 		double pi = 3.14;
 		double doirayuzi = pi * radius * radius;
 		double doiraperimetri = 2 * pi * radius;
