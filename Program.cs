c79df121a69cfa10fbdaf270361a5fc3c89176ff
Durdonaxon95 <qilichovadurdona7@gmail.com>
2025-02-01 09:05:37 +0500

    Add files via upload
    Signed-off-by: Durdonaxon95 <qilichovadurdona7@gmail.com>

--- Program.cs
+++ Program.cs
@@ -1,51 +1,12 @@
 ﻿using System;
 
-    class Program
-    {
-        static void Main()
-        {
-            // 1 dan 1000 gacha bo'lgan toq sonlarning yigindisini topish.
-            int yigindi = 0;
-            int son = 1;
-            while (son <= 1000)
-            {
-                yigindi += son;
-                son += 2;
-            }
-            Console.WriteLine("1 dan 1000 gacha bo'lgan toq sonlar yig'indisi: " + yigindi);
-            
-            //Array ichidagi eng katta va eng kichik qiymatlarni ko'paytmasini topish.
-            int[] sonlar = { 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15 };
-            int engKatta = sonlar[0];
-            int engKichik = sonlar[0];
-            for (int iterator = 1; iterator< sonlar.Length; iterator++)
-            {
-                if (sonlar[iterator] > engKatta)
-                {
-                    engKatta = sonlar[iterator];
-                }
-                if (sonlar[iterator] < engKichik)
-                {
-                    engKichik = sonlar[iterator];
-                }
-            }
-            Console.WriteLine("Eng katta qiymat: " + engKatta);
-            Console.WriteLine("Eng kichik qiymat: " + engKichik);
-            Console.WriteLine($"{engKichik} * {engKatta} = "+ (engKatta * engKichik));
+BookApp.Book book = new BookApp.Book("Buyuk Getsbi", "F.S. Fittsgerald", 1925, "Roman", 280, 12.5);
 
-            //x! faktorial x ni hisoblash.
-            Console.WriteLine("Faktorialni hisoblash uchun sonni kiriting: ");
-            int faktorial = Convert.ToInt32(Console.ReadLine());
-            int faktorialNatija = 1;
-            for (int iterator = 1; iterator <= faktorial; iterator++)
-            {
-                faktorialNatija *= iterator;
-            }
-            Console.WriteLine($"{faktorial}! = " + faktorialNatija);
+BookApp.Book book2 = new BookApp.Book("Alisher Navoiy","Oybek", 1999,"Roman", 300, 15.5);
+
+book.Display();
+
+book2.Display();
 
 
-    }
-    
-        
-    
-}
+
